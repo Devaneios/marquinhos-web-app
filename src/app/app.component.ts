a4c0b9f4b22823e48f0ecf65df289014e8f47261
Guilherme Afonso <gasp@cin.ufpe.br>
2023-08-13 03:50:22 -0300

    feat: add root page header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { User } from './types/user.interface';
+import { ProfileOptionsComponent } from './components/profile-options/profile-options.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    ProfileOptionsComponent,
+  ],
+  providers: [],
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
+    <header>
+      <img src="assets/logo.jpg" alt="logo" class="logo" />
+      <app-profile-options [user]="user"></app-profile-options>
+    </header>
     <router-outlet></router-outlet>
   `,
-  styles: [],
+  styles: [
+    `
+      :host {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        background-color: #36393f;
+
+        header {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          color: #7289da;
+          padding: 1rem 1.5rem;
+          background-color: #2f3136;
+          box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
+
+          .logo {
+            width: 3rem;
+            height: 3rem;
+            border-radius: 50%;
+          }
+        }
+      }
+    `,
+  ],
 })
-export class AppComponent {
-  title = 'marquinhos-web';
+export class AppComponent implements OnInit {
+  user: User | null = null;
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.router.events.forEach(() => {
+      this.user = JSON.parse(localStorage.getItem('user')!) || null;
+    });
+  }
 }

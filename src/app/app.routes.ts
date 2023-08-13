1d7eef7cc5503fd62591c7afac4742edc8cd4aee
Guilherme Afonso <gasp@cin.ufpe.br>
2023-08-13 03:50:31 -0300

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginDiscordComponent } from './pages/login/login-discord/login-discord.component';
+import { authGuard } from './core/auth/auth.guard';
+import { LoginLastFmComponent } from './pages/login/login-last-fm/login-last-fm.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'redirect-discord',
+    component: LoginDiscordComponent,
+  },
+  {
+    path: 'redirect-lastfm',
+    component: LoginLastFmComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+];

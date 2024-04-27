5b9572adfe767435b5a28922545964923717300f
Guilherme Afonso <guilhermeafonso208@gmail.com>
2024-04-27 12:07:44 -0300

    ci: move to firebase hosting

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/guilherme/github-runners/marquinhos-web-app-runner/_work/marquinhos-web-app/marquinhos-web-app/dist/marquinhos-web
-
-cp -R -f -v ./* /srv/devaneios
-
-echo "Deployed successfully!"

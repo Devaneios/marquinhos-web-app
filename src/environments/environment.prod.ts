f01fa88976fd976dedf7379da94f4f673da28306
Guilherme Afonso <guilhermeafonso208@gmail.com>
2024-04-28 15:04:26 -0300

    fix: update prod env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  host: 'https://marquinhosbot-9a236.web.app',
-  apiUrl: 'https://devaneios.guilhermeasper.dev.br:3105/api',
+  host: 'https://marquinhos-74154.web.app',
+  apiUrl: 'https://marquinhos.guilhermeasper.dev.br:3105/api',
 };

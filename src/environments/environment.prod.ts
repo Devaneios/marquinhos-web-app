cf19053f57da47063a7e58e9c3af85a8c3c46e81
Guilherme Afonso <gasp@cin.ufpe.br>
2024-09-01 12:26:59 -0300

    build: update prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   host: 'https://marquinhos-74154.web.app',
-  apiUrl: 'https://marquinhos.guilhermeasper.dev.br:3105/api',
+  apiUrl: 'https://dvns.guilhermeasper.dev.br:23417/api',
 };
